3421efbe8104758688c43cfb4a262427837238aa
Daniel Valencia Backhoff <daniel@helix.re>
2020-03-14 12:43:12 -0700

    ContactPage with contact form with validation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,9 +1,71 @@
+import {
+  Label,
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+} from '@redwoodjs/web'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <div>
-      <h1>ContactPage</h1>
-      <p>Find me in ./web/src/pages/ContactPage/ContactPage.js</p>
-    </div>
+    <BlogLayout>
+      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+        <Label
+          name="name"
+          errorStyle={{ display: 'block', color: 'red' }}
+          style={{ display: 'block' }}
+        >
+          Name
+        </Label>
+        <TextField
+          name="name"
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          validation={{ required: true }}
+        />
+        <FieldError name="name" style={{ color: 'red' }} />
+
+        <Label
+          name="email"
+          errorStyle={{ display: 'block', color: 'red' }}
+          style={{ display: 'block' }}
+        >
+          Email
+        </Label>
+        <TextField
+          name="email"
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^\.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+        />
+        <FieldError name="email" style={{ color: 'red' }} />
+
+        <Label
+          name="message"
+          errorStyle={{ display: 'block', color: 'red' }}
+          style={{ display: 'block' }}
+        >
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          errorStyle={{ display: 'block', borderColor: 'red' }}
+          validation={{ required: true }}
+        />
+        <FieldError name="message" style={{ color: 'red' }} />
+
+        <Submit style={{ display: 'block' }}>Save</Submit>
+      </Form>
+    </BlogLayout>
   )
 }
 
